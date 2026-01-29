4539df7a405ee036fec81836e5c106473f520a1c
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2026-01-29 10:50:26 +0100

    maj homapge

--- react-ts-vite-app/src/components/Header.tsx
+++ react-ts-vite-app/src/components/Header.tsx
@@ -2,11 +2,7 @@ import NavBar from "./NavBar"
 
 function Header() {
   return (
-<<<<<<< Updated upstream
-    <div className="text-center bg-[var(--color-brand)] text-white">
-=======
     <div className="text-center bg-[var(--color-primary)] text-white">
->>>>>>> Stashed changes
         <NavBar />
         <hr />
     </div>
